7c005af61691620ffd7bb099e321c3f21a4290f7
Mohammed Yasin <0310437@student.rocvantwente.nl>
2019-03-07 14:20:31 +0100

    opdracht 2.4

---
+++ opdracht_2.4/opdracht_2.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Opdracht_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string naam;
+            DateTime geboortedatum;
+
+            Console.WriteLine("Voer hier uw naam in: ");
+            naam = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("voer nu uw geboortedatum in ");
+            geboortedatum = Convert.ToString(Console.ReadLine());
+            Console.Write(DateTime.Now - geboortedatum);
+
+
+        }
+    }
+}
