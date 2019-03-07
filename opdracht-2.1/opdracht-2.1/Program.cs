ecd27ec7000e024473fccfe78a38676d31511732
Mohammed Yasin <0310437@student.rocvantwente.nl>
2019-03-07 14:18:02 +0100

    opdracht 2.1

---
+++ opdracht-2.1/opdracht-2.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Opdracht_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Mohammed Yasin");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Wederiklaan 48A");
+            Console.WriteLine("9549 DA Enshede");
+            Console.WriteLine("16 Jaar oud");
+            Console.ReadKey();
+        }
+    }
+}
